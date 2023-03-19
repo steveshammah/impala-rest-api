284fed5368089a3d2b518dffb133fb5f5875ce9d
Steve Shammah <71287999+steveshammah@users.noreply.github.com>
2023-03-19 12:05:00 +0300

    Update api endpoints (#3) (#5)
    * refactor: Update to class-based views
    
    * feat: add api endpoints
    
    - users, posts, authors, fixtures, teams, players, articles, products,
      fixtures and patners.
    
    Co-authored-by: Tony J <51135605+Gimex10@users.noreply.github.com>

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
+from django.views.generic import TemplateView
 
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('home.urls')),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/docs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]
 
 
